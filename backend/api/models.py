8c5f0ae6091d9afd52ed1cdfe0d1a3c8da627d3a
waltertaya <waltermittybecker5@gmail.com>
2024-10-04 18:07:11 +0300

    Add Job model to backend/api/models.py

---
+++ backend/api/models.py
@@ -0,0 +1,25 @@
+from . import db
+
+import datetime
+
+
+class Job(db.Model):
+
+    __tablename__ = 'jobs'
+
+    id = db.Column(db.Integer, primary_key=True, auto_increment=True)
+    title = db.Column(db.String(100), nullable=False)
+    location = db.Column(db.String(100), nullable=False)
+    salary = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.datetime.utcnow)
+    logo_src = db.Column(db.String(200), nullable=False)
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'location': self.location,
+            'salary': self.salary,
+            'date_posted': self.date_posted,
+            'logo_src': self.logo_src
+        }
