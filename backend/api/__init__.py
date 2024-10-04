f0ec167aa80414923c582efb5291394e87c08040
waltertaya <waltermittybecker5@gmail.com>
2024-10-04 17:54:14 +0300

    Refactor database initialization in create_app function

--- backend/api/__init__.py
+++ backend/api/__init__.py
@@ -1,6 +1,20 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+from config import Config
+
+
+db = SQLAlchemy()
 
 
 def create_app():
     app = Flask(__name__)
+
+    app.config.from_object(Config)
+    db.init_app(app)
+
+    with app.app_context():
+        from api import routes, models
+        db.create_all()
+
     return app
