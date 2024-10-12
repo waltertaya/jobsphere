379eb993ee6beb4722cd71226c89f6125cbfbbe9
waltertaya <waltermittybecker5@gmail.com>
2024-10-12 19:41:35 +0300

    Update API endpoint in JobListings component

--- backend/api/models.py
+++ backend/api/models.py
@@ -2,7 +2,6 @@ from . import db
 
 import datetime
 
-
 class Job(db.Model):
 
     __tablename__ = 'jobs'
@@ -14,6 +13,11 @@ class Job(db.Model):
     salary = db.Column(db.String(100), nullable=False)
     date_posted = db.Column(db.DateTime, nullable=False, default=datetime.datetime.utcnow)
     logo_src = db.Column(db.String(200), nullable=False)
+    company = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    requirements = db.Column(db.ARRAY(db.String), nullable=False)
+    apply_link = db.Column(db.String(200), nullable=False)
+    responsibilities = db.Column(db.ARRAY(db.String), nullable=False)
 
     def to_dict(self):
         return {
@@ -24,5 +28,9 @@ class Job(db.Model):
             'salary': self.salary,
             'date_posted': self.date_posted,
             'logoSrc': self.logo_src,
-            'postedDays': (datetime.datetime.utcnow() - self.date_posted).days
+            'postedDays': (datetime.datetime.utcnow() - self.date_posted).days,
+            'company': self.company,
+            'description': self.description,
+            'requirements': self.requirements,
+            'applyLink': self.apply_link,
         }
